0922c260cbec6f2b65b5ba45d5a70867422e313b
Victor de Sa Dantas <dev.victordantas@gmail.com>
2026-09-10 20:52:03 -0300

    enviando as alterações da criação de tabela de usuarios

--- db.py
+++ db.py
@@ -112,7 +112,7 @@ def criar_tabelas():
       id SERIAL PRIMARY KEY,
       nome VARCHAR(100) NOT NULL,
       email VARCHAR(100) NOT NULL UNIQUE,
-      senha VARCHAR(100) NOT NULL
+      senha_hash VARCHAR(100) NOT NULL
     );"""
   executar_sql(sql)
 
